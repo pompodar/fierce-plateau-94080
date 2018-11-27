5e83d19eb049d59ee48efb41ad22d1a929092282
pompodar <pompodar@gmail.com>
2018-11-27 13:32:03 +0200

    in c

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
-if(process.env.NoDE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
+    module.exports = {mongoURI: 
+        "mongodb://localhost/videodb"}
 module.exports = {mongoURI: 
     "mongodb://pompodar:10buboukrnet@ds115854.mlab.com:15854/diary"}
 }else{
-    module.exports = {mongoURI: 
-        "mongodb://localhost/videodb"}
+   
     
 }
